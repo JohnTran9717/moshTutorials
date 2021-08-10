e7e92fd60e20479375ca4b49a8b8826a68b9a4ea
JohnTran9717 <77471396+JohnTran9717@users.noreply.github.com>
2021-08-10 14:47:23 -0400

    Update basic.js

--- codewithmosh/basic.js
+++ codewithmosh/basic.js
@@ -16,16 +16,44 @@
 //     return ((width > height) ? "landscape" : "portrait");
 // }
 
-console.log(fizzbuzz(7))
+// console.log(fizzbuzz(7))
  
-function fizzbuzz(input) {
-    if ((input%3)==0 && (input%5)==0){
-        return("Fizzbuzz")
-    }else if((input%3==0) && (input%5!=0)){
-        return("Fizz")
-    }else if((input%3!=0) && (input%5==0)){
-        return("Buzz")
-    }else if((input%3!=0) || (input%5!=0) || (input%2!=0) || (input%2)!=1){
-        return(input)
-    }else return "Not a number"
+// function fizzbuzz(input) {
+//     if (typeof input !== "number"){
+//         return NaN
+//     }
+//     if ((input%3)==0 && (input%5)==0){
+//         return("Fizzbuzz")
+//     }
+//     if((input%3==0) && (input%5!=0)){
+//         return("Fizz")
+//     }
+//     if((input%3!=0) && (input%5==0)){
+//         return("Buzz")
+//     }
+//     if((input%2!=0) || (input%2)!=1){
+//         return(input)
+//     }
+// }
+
+
+
+checkspeed(180)
+
+function checkspeed(speed){
+    let speedLimit=70
+    let pointsPerMile =5
+    const point=Math.floor((speed-speedLimit)/pointsPerMile)-2
+    if (point <= 0){
+        console.log("Speed:" + point);
+        console.log("You're good")
+    }
+    if (point>=1 && point<10){
+        console.log("Speed:" + point);
+        console.log("You're getting a ticket")
+    }
+    if (point>=10){
+        console.log("Speed:"+ point)
+        console.log("Your going to jail")
+    }
 }
