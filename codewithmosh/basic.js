edb8c336534afc451b0cf95c7a6285d327fb2f2b
JohnTran9717 <77471396+JohnTran9717@users.noreply.github.com>
2021-08-12 13:11:22 -0400

    Update basic.js

--- codewithmosh/basic.js
+++ codewithmosh/basic.js
@@ -38,22 +38,68 @@
 
 
 
-checkspeed(180)
+// checkspeed(180)
 
-function checkspeed(speed){
-    let speedLimit=70
-    let pointsPerMile =5
-    const point=Math.floor((speed-speedLimit)/pointsPerMile)-2
-    if (point <= 0){
-        console.log("Speed:" + point);
-        console.log("You're good")
-    }
-    if (point>=1 && point<10){
-        console.log("Speed:" + point);
-        console.log("You're getting a ticket")
-    }
-    if (point>=10){
-        console.log("Speed:"+ point)
-        console.log("Your going to jail")
+// function checkspeed(speed){
+//     let speedLimit=70
+//     let pointsPerMile =5
+//     const point=Math.floor((speed-speedLimit)/pointsPerMile)-2
+//     if (point <= 0){
+//         console.log("Speed:" + point);
+//         console.log("You're good")
+//     }
+//     if (point>=1 && point<10){
+//         console.log("Speed:" + point);
+//         console.log("You're getting a ticket")
+//     }
+//     if (point>=10){
+//         console.log("Speed:"+ point)
+//         console.log("Your going to jail")
+//     }
+// }
+//  showNumbers(10)
+// function showNumbers(limit){
+//     if(typeof limit !== "number"){
+//         console.log(NaN)
+//     }else if(limit%2==0){
+//         console.log( limit+" " +'"Even"')
+//     }else {console.log(limit+' '+'"Odd"')}
+// }
+// displayEvenOrOdd(10)
+// function displayEvenOrOdd(limit){
+//     for(i=1; i<=limit; i++){
+//         if(i%2==0){
+//             console.log(i+' '+'"Even"')
+//         }else{
+//             console.log(i+' '+'"Odd"')
+//         }
+//     }
+// }
+// falsey undefined null '' false 0 NaN
+//truthy intergers strings true
+
+// const arrayInput =[1,2,3,4,5,true]
+// console.log(countTruthy())
+// console.log(arrayInput.length)
+// function countTruthy(){
+//     let count= 0;
+//     for(let value of arrayInput){
+//         if(value){count++}
+//     }
+//     return count
+// }
+let movie ={
+    title:'Another Movie',
+    releaseDate:'never',
+    rating:'unknown',
+    director:'No one',
+    length:3
+}
+showProperties(movie)
+function showProperties() {
+    for(let obj in movie){
+        if(typeof movie[obj] == 'string'){
+            console.log(obj, movie[obj])
+        }
     }
 }
